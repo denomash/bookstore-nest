58d804cbe5266e112587cbf7b43b4e40f58dc39b
Dennis Macharia <dennismacharia4747@gmail.com>
2019-09-13 13:00:38 +0300

    add crud functionality

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { BookDTO } from './dto/book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':id')
+  async getBook(@Param('id') bookId) {
+    const book = await this.booksService.getBook(bookId);
+    return book;
+  }
+
+  @Post()
+  async addBook(@Body() bookDTO: BookDTO) {
+    const book = await this.booksService.addBook(bookDTO);
+    return book;
+  }
+
+  @Delete()
+  async deteteBook(@Query() query) {
+    const books = await this.booksService.deleteBook(query.bookId);
+    return books;
+  }
+}
---
+++ src/books/dto/book.dto.ts
@@ -0,0 +1,8 @@
+// ./src/books/dto/create-book.dto.ts
+
+export class BookDTO {
+  readonly id: number;
+  readonly title: string;
+  readonly description: string;
+  readonly author: string;
+}
